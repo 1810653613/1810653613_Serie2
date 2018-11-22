37bc959f163dd5291c70cdad1b5dbd8aee67ac38
Joachimstaller Maximilian <1810653613@stud.fh-kufstein.ac.at>
2018-11-22 11:17:41 +0100

    Aufgabe 2

--- src/Zahlenraten.java
+++ src/Zahlenraten.java
@@ -7,13 +7,19 @@ public class Zahlenraten
     {
         int min=1;
         int max=10;
+
         Random rnd= new Random();
         int zufallszahl = rnd.nextInt(max-min+1)+min;
         /* Dieser Code definiert die zufallszahl durch eine Gleichung*/
         System.out.println(zufallszahl);
-        if(zufallszahl/2=0)
+
+        if((zufallszahl%2)==0)
         {
-            JOptionPane.showMessageDialog(null, "");
+         JOptionPane.showMessageDialog(null, "Die Zahl is gerade.");
+        }
+        else
+        {
+            JOptionPane.showMessageDialog(null,"Die Zahl ist ungerade.");
         }
     }
 }
